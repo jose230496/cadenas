cd49402b90acefb49f26608098c1b130e0ab8052
josevillanueva <151975312+jose230496@users.noreply.github.com.>
2023-12-14 07:55:31 -0500

    juego del ahorcado

---
+++ ciclos.py
@@ -0,0 +1,30 @@
+import os
+os.system('cls')
+mensaje = "fundamentos de programacion"
+contadorConsonantes =int(0)
+vocales =int(0)
+lstvocales = " a,e,i,o,u"
+for caracter in mensaje
+if caracter in lstvocales
+          vocales +=1
+elif caracter.isalpha
+if 'a' in "a,e,i,o,u":
+    print ('hay vocales')
+    if 'a' in "a,b,c,d,e,f,g,h,i,j,k,l,ll,m,n,o,p,q,r,s,t,u,v,w,x,y,z":
+        print ('hay consonantes ')
+
+        else
+        consonantes +=1
+        print(f"el total de vocales es = {vocales}")
+        print(f"el total de las consonantes es = {consonantes} ")
+        for item in range (0.5)
+        print(iten)
+
+        for i,item in  enumerate (mensaje)
+        print(f"Pos {i} - {item}")
+        mensaje. replace(item)
+        print
+
+
+#for caracter in mensaje
+#   print (caracter)
---
+++ proyecto.py
@@ -0,0 +1,15 @@
+mensaje = " Bienvenidos a python  2023"
+valor = "3444"
+print(f"{mensaje.capitalize()}")
+print(f"{mensaje.upper()}")
+print(f"{mensaje.lower()}")
+print(f"{mensaje.swapcase()}")
+print(f"{mensaje.count('a')}")
+print(f"{mensaje.count('b')}")
+print(f"{len(mensaje)()}")
+print(f"{mensaje.isalnum}")
+print(f"{mensaje.isalpha()}")
+print(f"{mensaje.zfill}")
+
+
+
